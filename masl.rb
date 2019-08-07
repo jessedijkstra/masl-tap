4420ea2de4cc1adf332744543a234112607848c7
Jesse Dijkstra <mail@jessedijkstra.nl>
2019-08-07 16:48:56 +0200

    Version updated to 2.0.0

--- masl.rb
+++ masl.rb
@@ -1,12 +1,12 @@
 class Masl < Formula
     desc "Assume an AWS Role using Onelogin"
     homepage "https://github.com/glnds/masl"
-    url "https://github.com/glnds/masl/releases/download/1.2.7/masl-v1.2.7-darwin-amd64"
-    sha256 "f6a56b7dcb84c65d1137be7c4f25ba65e8b2b9d285c61ea64d0c4c4aab22fbca"
-    version "1.2.7"
+    url "https://github.com/glnds/masl/releases/download/2.0.0/masl-v2.0.0-darwin-amd64"
+    sha256 "f1d87f756aa4b24cdc2f71e49d0aadec244d18aee960e22eab8e9b23decfc957"
+    version "2.0.0"
   
     def install
-      mv "masl-v1.2.7-darwin-amd64", "masl"
+      mv "masl-v2.0.0-darwin-amd64", "masl"
       bin.install "masl"
     end
 end
